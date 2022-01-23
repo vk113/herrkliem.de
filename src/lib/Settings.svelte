68baa96ab57d19883398312f121f96727d9eae7d
Vincent Kliem <vincent.kliem@gmail.com>
2022-01-23 19:15:47 +0100

    add input type number to settings

--- src/lib/Settings.svelte
+++ src/lib/Settings.svelte
@@ -21,9 +21,15 @@
         Magnetfelder
     </h3>
     {#each simulation.b_fields as b_field, i}
-        <div>
-            B<sub>{i+1}</sub> = {String(b_field.B).replace(".", ",")} T
-        </div>
+    <div>
+        <span>
+            B<sub>{i+1}</sub> = 
+        </span>
+        <input class="w-18 bg-gray-200" type="number" bind:value={b_field.B}  min={$B_min} max={$B_max} step={$B_step}>
+        <span>
+            T
+        </span>
+    </div>
         <input bind:value={b_field.B} type="range" min={$B_min} max={$B_max} step={$B_step}>
         <button class="text-red-500" on:click={() => simulation.b_fields.splice(i, 1)}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -38,7 +44,13 @@
     </h3>
     {#each simulation.e_fields as e_field, i}
         <div>
-            E<sub>{i+1}</sub> = {String(e_field.E.y).replace(".", ",")} V/m
+            <span>
+                E<sub>{i+1}</sub> = 
+            </span>
+            <input class="w-18 bg-gray-200" type="number" bind:value={e_field.E.y}  min={$E_min} max={$E_max} step={$E_step}>
+            <span>
+                V/m
+            </span>
         </div>
         <input bind:value={e_field.E.y} type="range" min={$E_min} max={$E_max} step={$E_step}>
         <button class="text-red-500" on:click={() => simulation.e_fields.splice(i, 1)}>
--- src/lib/settings.js
+++ src/lib/settings.js
@@ -4,8 +4,8 @@ export const e_in_front = writable(true);
 
 export const B_min = writable(-1);
 export const B_max = writable(1);
-export const B_step = writable(0.01);
+export const B_step = writable(0.001);
 export const E_min = writable(-2);
 export const E_max = writable(2);
-export const E_step = writable(0.01);
+export const E_step = writable(0.001);
   
