7a0f533be35be0759eb4003424ecf720ae6ca412
Vincent Kliem <vincent.kliem@gmail.com>
2022-07-11 11:31:35 +0200

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
+	  // default options are shown
+	  adapter: vercel({
+		// if true, will deploy the app using edge functions
+		// (https://vercel.com/docs/concepts/functions/edge-functions)
+		// rather than serverless functions
+		edge: false,
+  
+		// an array of dependencies that esbuild should treat
+		// as external when bundling functions
+		external: [],
+  
+		// if true, will split your app into multiple functions
+		// instead of creating a single one for the entire app
+		split: false
+	  })
 	}
-};
-
-export default config;
+  };
+  
