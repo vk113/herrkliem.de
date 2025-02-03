48ddef60686601881643f70ae447a9b19dc5d94a
vk113 <42933261+vk113@users.noreply.github.com>
2025-02-03 22:54:35 +0100

    elektronen fix

--- src/routes/app/elektronenstrahl/+page.svelte
+++ src/routes/app/elektronenstrahl/+page.svelte
@@ -5,18 +5,18 @@
     let simulation = new ph.Simulation(
 		new ph.Source(100, 500),
 		[],
-		[new ph.EField(100, 100, 200, 200, {x: 0, y: 1000})],
-		[new ph.BField(100, 100, 200, 200, .01)],
+		[new ph.EField(100, 100, 200, 200, {x: 0, y: 10})],
+		[new ph.BField(100, 100, 200, 200, .0001)],
 	);
 	simulation.colors = ["blue", "blue", "blue"];
     simulation.vs = [2*10**5];
-    simulation.ms = [9.109e-29];
-    $B_min = -0.01;
-    $B_max = 0.01;
+    simulation.ms = [9.109e-31];
+    $B_min = -0.001;
+    $B_max = 0.001;
     $B_step = 0.00001;
-    $E_min = -3000;
-    $E_max = 3000;
-    $E_step = 1;
+    $E_min = -50;
+    $E_max = 50;
+    $E_step = 0.1;
     
 </script>
 
